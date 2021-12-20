968e33d361447fe744a1a8946c8575542ebc244a
Utkarsh736 <utkarshtomar736@gmail.com>
2021-12-20 20:35:30 +0530

    Styling for blog page

--- flaskblog.py
+++ flaskblog.py
@@ -1,8 +1,23 @@
-from flask import Flask
+from flask import Flask, render_template, url_for
 
 # __name__ is name of module (spl. var. in python)
 
 app = Flask(__name__)
+
+posts = [
+    {
+        'author': 'Utkarsh Tomar',
+        'title': 'Blog Post 1',
+        'content': 'First post content',
+        'date_posted': 'December 20, 2021'
+    },
+    {
+        'author': 'John Doe',
+        'title': 'Blog Post 2',
+        'content': 'Second post content',
+        'date_posted': 'December 21, 2021'
+    }
+]
 
 # Creating routes for differnet pages
 # Using Decorators
@@ -10,12 +25,12 @@ app = Flask(__name__)
 @app.route("/") # Home page
 @app.route("/home")
 def home():
-    return "<h1>Home Page</h1>"
+    return render_template('home.html', posts=posts)
 
 
 @app.route("/about") # About page
 def about():
-    return "<h1>About Page</h1>"
+    return render_template('about.html', title='About')
 
 if __name__ == '__main__':
     app.run(debug=True)
