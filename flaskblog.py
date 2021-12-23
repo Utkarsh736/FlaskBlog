75128cbb7d6ca4684186c20d8a0531997577e56b
Utkarsh736 <utkarshtomar736@gmail.com>
2021-12-23 16:24:50 +0530

    Form validation + rerouting + invalid check

--- flaskblog.py
+++ flaskblog.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, url_for
+from flask import Flask, render_template, url_for, flash, redirect
 from forms import RegistrationForm, LoginForm
 
 # __name__ is name of module (spl. var. in python)
@@ -40,6 +40,9 @@ def about():
 @app.route("/register", methods=['GET', 'POST']) # Registration Form
 def register():
     form = RegistrationForm()
+    if form.validate_on_submit():
+        flash(f'Account created for {form.username.data}!', 'success')
+        return redirect(url_for('home'))
     return render_template('register.html', title='Register', form=form)
 
 
