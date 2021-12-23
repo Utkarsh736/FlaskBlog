f039f9ec463553abf272aa85b46c770624f0d9f3
Utkarsh736 <utkarshtomar736@gmail.com>
2021-12-23 14:38:52 +0530

    Secret key + from routes

--- flaskblog.py
+++ flaskblog.py
@@ -1,8 +1,12 @@
 from flask import Flask, render_template, url_for
+from forms import RegistrationForm, LoginForm
 
 # __name__ is name of module (spl. var. in python)
 
 app = Flask(__name__)
+
+app.config['SECRET_KEY'] = '4d02ba5bcbaee679dd80e78cba5644b7'
+
 
 posts = [
     {
@@ -31,6 +35,18 @@ def home():
 @app.route("/about") # About page
 def about():
     return render_template('about.html', title='About')
+
+
+@app.route("/register") # About page
+def register():
+    form = RegistrationForm()
+    return render_template('register.html', title='Register', form=form)
+
+
+@app.route("/login") # About page
+def login():
+    form = LoginForm()
+    return render_template('login.html', title='Login', form=form)
 
 if __name__ == '__main__':
     app.run(debug=True)
