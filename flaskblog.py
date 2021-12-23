2766d8e9b8315dad0abf5615c7f94aa253d5cea0
Utkarsh736 <utkarshtomar736@gmail.com>
2021-12-23 16:39:37 +0530

    Login route method added

--- flaskblog.py
+++ flaskblog.py
@@ -46,9 +46,15 @@ def register():
     return render_template('register.html', title='Register', form=form)
 
 
-@app.route("/login") # Login Form
+@app.route("/login", methods=['GET', 'POST']) # Login Form
 def login():
     form = LoginForm()
+    if form.validate_on_submit():
+        if form.email.data == 'admin@blog.com' and form.password.data == 'password':
+            flash('You have been logged in!', 'success')
+            return redirect(url_for('home'))
+        else:
+            flash('Login Unsuccessful, Please check username and password', 'danger')
     return render_template('login.html', title='Login', form=form)
 
 if __name__ == '__main__':
