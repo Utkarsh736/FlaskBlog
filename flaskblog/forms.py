915a886a1762ce419ba32b40742dc00fd33afe67
Utkarsh736 <utkarshtomar736@gmail.com>
2021-12-30 16:18:37 +0530

    User account update

--- flaskblog/forms.py
+++ flaskblog/forms.py
@@ -1,4 +1,6 @@
 from flask_wtf import FlaskForm
+from flask_wtf.file import FileField, FileAllowed
+from flask_login import current_user
 from wtforms import StringField, PasswordField, SubmitField, BooleanField
 from wtforms.validators import DataRequired, Length, Email, EqualTo, ValidationError
 from flaskblog.models import User
@@ -53,3 +55,35 @@ class LoginForm(FlaskForm):
 
     # Submit Field
     submit = SubmitField('Login')
+
+
+
+class UpdateAccountForm(FlaskForm):
+    # Username Field
+    username = StringField('Username', 
+                            validators=[DataRequired(), Length(min=2, max=20)])
+
+    # Email Field
+    email  = StringField('Email',
+                        validators=[DataRequired(), Email()])
+
+    picture = FileField('Update Profile Picture', validators=[FileAllowed(['jpg', 'png'])])
+
+    # Submit Field
+    submit = SubmitField('Update')
+
+    # Validating new account
+    def validate_username(self, username):
+        if username.data != current_user.username:
+            user = User.query.filter_by(username=username.data).first()
+
+            if user:
+                raise ValidationError('That username is taken. Please choose another username')
+
+    
+    def validate_email(self, email):
+        if email.data != current_user.email:
+            user = User.query.filter_by(email=email.data).first()
+
+            if user:
+                raise ValidationError('That email is taken. Please choose another email')
