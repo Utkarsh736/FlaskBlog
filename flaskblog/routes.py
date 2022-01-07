79c2e78630d743b8beb0ad587731a2c45106ffbc
Utkarsh736 <utkarshtomar736@gmail.com>
2022-01-07 21:13:25 +0530

    Pagination setup

--- flaskblog/routes.py
+++ flaskblog/routes.py
@@ -14,7 +14,8 @@ from flask_login import login_user, current_user, logout_user, login_required
 @app.route("/") # Home page
 @app.route("/home")
 def home():
-    posts = Post.query.all()
+    page = request.args.get('page', 1, type=int)
+    posts = Post.query.paginate(page=page, per_page=5)
     return render_template('home.html', posts=posts)
 
 
