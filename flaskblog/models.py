df5382789135bc2f68cddfd6cbf56e48e2e077fe
Utkarsh736 <utkarshtomar736@gmail.com>
2022-01-16 13:00:04 +0530

    importing Serializer module

--- flaskblog/models.py
+++ flaskblog/models.py
@@ -1,5 +1,6 @@
 from datetime import datetime
-from flaskblog import db, login_manager
+from itsdangerous import TimedJSONWebSignatureSerializer as Serializer
+from flaskblog import db, login_manager, app
 from flask_login import UserMixin
 
 @login_manager.user_loader
