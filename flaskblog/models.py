b2ba4606ca6c0b0a1d57c921b7f8291ab72cb8e2
Utkarsh736 <utkarshtomar736@gmail.com>
2022-01-16 16:09:36 +0530

    [Fixed]dict object has no attribute decode

--- flaskblog/models.py
+++ flaskblog/models.py
@@ -20,7 +20,7 @@ class User(db.Model, UserMixin):
 
     def get_reset_token(self, expires_sec=1800):
         s = Serializer(app.config['SECRET_KEY'], expires_sec)
-        return s.dumps({'user_id': self.id}.decode('utf-8'))
+        return s.dumps({'user_id': self.id}).decode('utf-8')
 
 
     @staticmethod
